d555e0c4a5b6d23c776f91d042c66783bb730c62
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-05-05 23:57:26 -0300

    feat: implementa página inicial de login

--- public/index.php
+++ public/index.php
@@ -7,6 +7,28 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    
+    <div class="bg-gray-100 ">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+              <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                      Faça Login na sua conta!
+                  </h1>
+                  <form class="space-y-4 md:space-y-6" action="#">
+                      <div>
+                          <label for="nome_usuario" class="block mb-2 text-sm font-medium text-gray-900 ">Nome de usuário</label>
+                          <input type="text" name="nome_usuario" id="nome_usuario" class="bg-gray-50 border-2 border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 outline-none" placeholder="pedro_silva" required="">
+                      </div>
+                      <div>
+                          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                          <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border-2 border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 outline-none" required="">
+                      </div>
+                      <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer">Login</button>
+                      
+                  </form>
+              </div>
+          </div>
+      </div>
+    </div> 
 </body>
 </html>
