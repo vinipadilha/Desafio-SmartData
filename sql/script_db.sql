55f44b4dfb9dfa01fc31539cbc92241d0aee1ed0
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-05-06 00:28:43 -0300

    feat: cria estrutura inicial do banco de dados

---
+++ sql/script_db.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS sistema_gerenciamento_clientes;
+
+USE sistema_gerenciamento_clientes;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(50) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    data_cadastro_usuario TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    documento VARCHAR(20),
+    telefone VARCHAR(20),
+    email VARCHAR(100),
+    endereco VARCHAR(255),
+    id_usuario INT NOT NULL, 
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+
+SELECT * FROM usuarios;
+SELECT * FROM clientes;
